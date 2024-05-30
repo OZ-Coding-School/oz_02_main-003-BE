7287f54b69f8446fa6ec10d1a7275b909cba14b1
Kim <rlawotjq206@gmail.com>
2024-05-30 18:03:50 +0900

    Feat: models create (#3)

--- app/alerts/models.py
+++ app/alerts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import User
+from recipes.models import Recipe
+from common.models import CommonDateModel
 
 # Create your models here.
+
+
+class Alert(CommonDateModel):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    
+    type = models.PositiveIntegerField(null=True, blank=True)
+    status = models.BooleanField(null=True, default=None)
--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from common.models import CommonDateModel
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+
+        return self.create_user(email, name, password, **extra_fields)
+
+
+class User(AbstractBaseUser, CommonDateModel):
+    """시스템 내 개별 사용자를 나타내는 사용자 모델입니다."""
+
+    email = models.EmailField(unique=True, null=True, blank=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    gender = models.BooleanField(default=False, null=True, blank=True)
+    age = models.PositiveIntegerField(null=True, default=None)
+    nickname = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    image = models.URLField(null=True, default=None, blank=True)
+    is_staff = models.BooleanField(default=False, null=True, blank=True)
+    is_login = models.BooleanField(default=False, null=True, blank=True)
+    is_alert = models.BooleanField(default=False, null=True, blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def __str__(self):
+        return self.email
+
+
+class User_refresh_token(CommonDateModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=255, null=True, blank=True)
+    estimate = models.DateTimeField(auto_now_add=True, null=True, blank=True)
