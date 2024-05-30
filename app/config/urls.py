e7c22b294c72c06e6ca611a272356c857f3b4b61
[Kang jeongho] <rkdwjdgh04@naver.com>
2024-05-30 14:19:44 +0900

    Chore: url 주소 지정 및 common 모델 생성 (#1)

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,22 +1,21 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # swagger-ui
+    path('api/v1/schema', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/schema/swagger', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/schema/redoc', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    # applications
+    path("admin", admin.site.urls),
+    path("api/v1/alerts", include("alerts.urls")),
+    path("api/v1/bookmarks", include("bookmarks.urls")),
+    path("api/v1/comments", include("comments.urls")),
+    path("api/v1/fridges", include("fridges.urls")),
+    path("api/v1/ingredients", include("ingredients.urls")),
+    path("api/v1/likes", include("likes.urls")),
+    path("api/v1/main", include("main.urls")),
+    path("api/v1/recipes", include("recipes.urls")),
+    path("api/v1/users", include("users.urls")),
 ]
