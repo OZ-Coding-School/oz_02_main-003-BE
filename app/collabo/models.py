aa644ce72824c4d1b9a6d78a9e8831cb528a67c7
Kim <rlawotjq206@gmail.com>
2024-05-31 14:11:32 +0900

    Feat: create collabo model

--- app/collabo/models.py
+++ app/collabo/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from common.models import CommonDateModel
+from users.models import User
+from ingredients.models import Ingredient
+from recipes.models import Recipe
 
-# Create your models here.
+
+class Fridge(CommonDateModel):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_fridges"
+    )
+    ingredient = models.ForeignKey(
+        Ingredient, on_delete=models.CASCADE, related_name="ingredient_fridges"
+    )
+
+    def __str__(self):
+        return f"{self.user}'s fridge contains {self.ingredient}"
+
+
+class Interaction(CommonDateModel):
+    INTERACTION_TYPE_CHOICES = [
+        (1, "Click"),
+        (2, "Favorite"),
+        (3, "Like"),
+        (4, "Comment"),
+    ]
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_interactions"
+    )
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="recipe_interactions"
+    )
+    type = models.PositiveIntegerField(choices=INTERACTION_TYPE_CHOICES)
+
+    def __str__(self):
+        return f"{self.get_type_display()} by {self.user} on {self.recipe}"
