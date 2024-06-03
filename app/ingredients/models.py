4c84cf990dfbf9463acf5eb781cb8c19dcb6581c
[Kang jeongho] <rkdwjdgh04@naver.com>
2024-06-03 13:35:06 +0900

    Feat: 레시피 수정 API 작성 및 재료 모델 변경 (#17)

--- app/ingredients/models.py
+++ app/ingredients/models.py
@@ -4,5 +4,6 @@ from common.models import CommonDateModel
 
 class Ingredient(CommonDateModel):
 
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, null=True, blank=True)
     image = models.URLField(null=True, default=None)
