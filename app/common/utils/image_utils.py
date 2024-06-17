2836711c6b56d0e3d270c0a7b0ae1891db14f303
newbission <newbission@gmail.com>
2024-06-17 22:35:03 +0900

    HOTFIX: 이미지 필드는 url 사용

--- app/common/utils/image_utils.py
+++ app/common/utils/image_utils.py
@@ -3,8 +3,5 @@ from config.settings import MEDIA_URL, BUCKET_PATH
 def get_image_uri(image_uri):
     if image_uri:
         image_uri = image_uri.strip()
-        if BUCKET_PATH in image_uri:
-            return image_uri
-        else:
-            return MEDIA_URL + image_uri
-    return None
+        
+    return MEDIA_URL + image_uri if image_uri else None
